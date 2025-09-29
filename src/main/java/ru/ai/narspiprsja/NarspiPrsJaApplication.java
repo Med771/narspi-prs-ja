635c5d8cb65e966c61b81115e6788007c287708e
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-09-29 09:29:58 +0300

    Int commit

---
+++ src/main/java/ru/ai/narspiprsja/NarspiPrsJaApplication.java
@@ -0,0 +1,13 @@
+package ru.ai.narspiprsja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarspiPrsJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarspiPrsJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ai/narspiprsja/NarspiPrsJaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ai.narspiprsja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarspiPrsJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
